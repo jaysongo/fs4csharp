f39bff3f5b15c92e8ee3e26e77c534a872cd643b
jgo <jayson.go@gmail.com>
2019-03-09 09:16:17 -0700

    Initial Code - New Map methods and tests

---
+++ src/FS4CSharp/MapExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FS4CSharp
+{
+    public static class MapExtensions
+    {
+        public static TOutput Map<TInput, TOutput>(this TInput input, Func<TInput, TOutput> map) => map(input);
+
+        public static TInput Map<TInput>(this TInput input, Action map)
+        {
+            map();
+            return input;
+        }
+    }
+}
---
+++ test/MapExtensionsTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using FS4CSharp;
+
+namespace FS4CSharp.Tests
+{
+    public class MapExtensionsTests
+    {
+        [Fact]
+        public void MapUsage1()
+        {
+            Func<double, double> CelsiusToFahrenheit = c =>
+                c * (9/5D) + 32;
+
+            Func<double, double> FahrenheitToCelsius = f =>
+                (f - 32) / (9/5D);
+
+            const double zeroCelsius = 0;
+            var result = zeroCelsius
+                .Map(CelsiusToFahrenheit)
+                .Map(FahrenheitToCelsius);
+            Assert.Equal(zeroCelsius, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void MapsIntegersToStrings(int input)
+        {
+            var result = input.Map(n => n.ToString());
+            Assert.Equal(input.ToString(), result);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("0")]
+        [InlineData("1")]
+        public void MapsStringsToIntegers(string input)
+        {
+            var result = input.Map(s => int.Parse(s));
+            Assert.Equal(int.Parse(input), result);
+        }
+
+        [Fact]
+        public void MapsToSelf()
+        {
+            var input = "abc";
+            var number = 0;
+
+            var output = input.Map(() => number++);
+
+            Assert.Same(input, output);
+            Assert.Equal(1, number);
+        }
+    }
+}
